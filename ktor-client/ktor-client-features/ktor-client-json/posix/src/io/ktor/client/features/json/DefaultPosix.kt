23535ca8a962da669959c33b2f2658bdbe9ca44f
Leonid Stashevsky <leonid.stashevsky@gmail.com>
2019-03-14 12:57:51 +0300

    Move KotlinxSerializer dependency in separate artifact

--- ktor-client/ktor-client-features/ktor-client-json/posix/src/io/ktor/client/features/json/DefaultPosix.kt
+++ ktor-client/ktor-client-features/ktor-client-json/posix/src/io/ktor/client/features/json/DefaultPosix.kt
@@ -2,4 +2,7 @@ package io.ktor.client.features.json
 
 import io.ktor.client.features.json.serializer.*
 
+/**
+ * Platform default serializer.
+ */
 actual fun defaultSerializer(): JsonSerializer = KotlinxSerializer()
